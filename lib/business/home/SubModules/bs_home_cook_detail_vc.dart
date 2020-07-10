9376499c2d673013eceb533866cbb19204b4d3ff
WuZhen <1833361588@qq.com>
2020-07-10 20:13:17 +0800

    add:scrollcontroller

--- lib/business/home/SubModules/bs_home_cook_detail_vc.dart
+++ lib/business/home/SubModules/bs_home_cook_detail_vc.dart
@@ -21,13 +21,66 @@ class LTCookRecipeDetailVc extends StatefulWidget {
 
 class _LTCookRecipeDetailVcState extends State<LTCookRecipeDetailVc> {
 
+  ScrollController _controller = new ScrollController();
+  double offset = 0;
+
+  @override
+  void initState() {
+    _controller.addListener(() {
+        print(_controller.offset);
+        if(_controller.offset < 0) {
+          setState(() {
+            offset = - _controller.offset;
+          });
+        }
+
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: recipeDetailInfoWidget(),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Positioned(
+                left: 0,
+                top: 0,
+                right: 0,
+                child: SizedBox(
+                  height: 250 + offset,
+                  child: Container(
+                    decoration: BoxDecoration( color: Colors.red),
+                    child: imageBackgroudWidget(),
+                  ),
+                )
+            ),
+            Positioned(
+              top: 0,
+              right: 0,
+              bottom: 0,
+              left: 0,
+              child: recipeDetailInfoWidget(),
+            )
+          ],
+
+        ),
+      ),
     );
   }
 
+  /// 图片背景
   Widget imageBackgroudWidget() {
     return FadeInImage.assetNetwork(
         placeholder: "",
@@ -36,16 +89,16 @@ class _LTCookRecipeDetailVcState extends State<LTCookRecipeDetailVc> {
     );
   }
 
+  /// 菜谱信息 widget
   Widget recipeDetailInfoWidget() {
-     return Expanded(
-       child: ListView.builder(itemBuilder: (BuildContext context, int index){
-         if(index == 0) {
-           return cellClearTop();
-         }
-         return cellInfo();
-       },
-         itemCount: 2,
-       ),
+     return ListView.builder(itemBuilder: (BuildContext context, int index){
+       if(index == 0) {
+         return cellClearTop();
+       }
+       return cellInfo();
+     },
+       itemCount: 2,
+       controller: _controller,
      );
   }
 
@@ -55,7 +108,7 @@ class _LTCookRecipeDetailVcState extends State<LTCookRecipeDetailVc> {
       height: 200,
       width: 300,
       child: Container(
-        decoration: BoxDecoration( color: Colors.red),
+        decoration: BoxDecoration( color: Colors.red.withAlpha(0)),
       ),
     );
   }
@@ -63,37 +116,66 @@ class _LTCookRecipeDetailVcState extends State<LTCookRecipeDetailVc> {
   /// 菜谱信息
   Widget cellInfo() {
     return  Container(
-      decoration: BoxDecoration( color: Colors.green),
-      child: SizedBox(
-        width: kScreenWidth,
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              titleInfoCloumn(),
-              setupsBackgroudWidget(),
-            ],
-          ),
+      decoration: BoxDecoration( color: Colors.white),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            titleInfoCloumn(),
+            materialsBackgroudWidget(),
+            setupsBackgroudWidget(),
+          ],
         ),
       ),
     );
   }
 
-  Widget setupsBackgroudWidget() {
-      return  Expanded(
-        child: Container(
-          decoration: BoxDecoration(color: Color(0xFFF5F5F7)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text("需要材料", style: TextStyle(color: Color(0xFF1E192F), fontSize: 18),),
-              setupsWidgetColumn()
-            ],
-          ),
+  /// 材料背景
+  Widget materialsBackgroudWidget() {
+      return Container(
+        decoration: BoxDecoration(color: Color(0xFFF5F5F7)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("需要材料", style: TextStyle(color: Color(0xFF1E192F), fontSize: 18),),
+            materialsWidgetColumn()
+          ],
         ),
       );
+  }
+
+  Widget materialsWidgetColumn() {
+    List<Widget> setupWidgets = new List();
+    for (int i = 0; i < 11; i++) {
+      setupWidgets.add(materialsWidget("步骤一", "1"));
+    }
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children:setupWidgets,
+    );
+  }
+
+  Widget materialsWidget(String title, String subTitle) {
+    return Row(
+      children: <Widget>[
+      Text(title, style: TextStyle(color: Color(0xFF84838B), fontSize: 18),),
+      ],
+    );
+  }
+
+  /// 步骤背景
+  Widget setupsBackgroudWidget() {
+     return Column(
+       mainAxisAlignment: MainAxisAlignment.start,
+       crossAxisAlignment: CrossAxisAlignment.start,
+       children: <Widget>[
+         Text("烹饪步骤", style: TextStyle(color: Color(0xFF1E192F), fontSize: 18),),
+         setupsWidgetColumn()
+       ],
+     );
   }
 
   Widget setupsWidgetColumn() {
@@ -104,12 +186,27 @@ class _LTCookRecipeDetailVcState extends State<LTCookRecipeDetailVc> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
-      children:setupWidgets
+      children:setupWidgets,
     );
   }
 
   Widget setupsWidget(String title, String subTitle) {
-    return Text(title, style: TextStyle(color: Color(0xFF84838B), fontSize: 18),);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("1/3", style: TextStyle(color: Color(0xff84838B), fontSize: 16),),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 12, 0, 12),
+          child: FadeInImage.assetNetwork(
+              placeholder: "",
+              fit: BoxFit.cover,
+              image: widget.model.uImageSrc
+          ),
+        ),
+        Text("鲫鱼收拾干净 表面划几刀易于入味"),
+      ],
+    );
   }
 
   /// 用户信息描述
